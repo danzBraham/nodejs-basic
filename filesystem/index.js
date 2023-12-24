7a5dcee34699a0604355d091e515a919dc745b39
Zidan Abraham <zidangit07@gmail.com>
2023-12-24 15:50:43 +0800

    chore: refreshing lesson

--- filesystem/index.js
+++ filesystem/index.js
@@ -1,22 +1,33 @@
-import fs from "fs";
-import path from "path";
+import { promisify } from 'util';
+import fs from 'fs';
+import path from 'path';
 
 const readFileCallBack = (data, error) => {
-   return new Promise((resolve, reject) => {
-      setTimeout(() => {
-         if (data) {
-            resolve(data);
-         } else if (error) {
-            reject(error);
-         }
-      }, 500);
-   }).then((result) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (data) {
+        resolve(data);
+      } else if (error) {
+        reject(error);
+      }
+    }, 500);
+  })
+    .then((result) => {
       console.log(result);
-   }).catch((err) => {
+    })
+    .catch((err) => {
       console.log(err);
-   });
-}
+    });
+};
 
-const address = path.resolve("./filesystem", "notes.txt");
+const address = path.resolve('./filesystem', 'notes.txt');
+// fs.readFile(address, 'UTF-8', readFileCallBack);
 
-fs.readFile(address, "UTF-8", readFileCallBack);
+// Promisify fs.readFile so that Promise-based
+const readFilePromise = promisify(fs.readFile);
+setTimeout(() => {
+  readFilePromise(address, 'utf-8')
+    .then((data) => console.log(data))
+    .catch((err) => console.error(err.message));
+}, 1000);
+console.log('Reading file...');
--- index.js
+++ index.js
@@ -1,21 +1,37 @@
-const message = name => {
-   console.log(`Hello ${name}, welcome to JavaScript!`);
+// const message = name => {
+//    console.log(`Hello ${name}, welcome to JavaScript!`);
+// }
+
+// message("Zidan");
+
+// const cpu = process.memoryUsage();
+// const pwd = process.env.PWD;
+
+// console.log(cpu);
+// console.log(pwd);
+
+// const first = process.argv[0];
+// const second = process.argv[1];
+// const third = process.argv[2];
+// const fourth = process.argv[3];
+
+// console.log("\n");
+// console.log(first);
+// console.log(second);
+// console.log(`Halo ${third} ${fourth}`);
+
+function getUser() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const people = ['Arthur', 'Dutch', 'John'];
+      // const people = null;
+      if (!people) reject(new Error('No people'));
+      resolve(people);
+    }, 3000);
+    console.log('Read Dead Redemption 2');
+  });
 }
 
-message("Zidan");
-
-const cpu = process.memoryUsage();
-const pwd = process.env.PWD;
-
-console.log(cpu);
-console.log(pwd);
-
-const first = process.argv[0];
-const second = process.argv[1];
-const third = process.argv[2];
-const fourth = process.argv[3];
-
-console.log("\n");
-console.log(first);
-console.log(second);
-console.log(`Halo ${third} ${fourth}`);
+getUser()
+  .then((people) => console.log(people))
+  .catch((err) => console.error(err.message));
